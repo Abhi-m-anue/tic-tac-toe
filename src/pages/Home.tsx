e684489f3bb0e285ee2ae9eb27ac109d3bc6f3e6
Abhimanue <abhimanue80@gmail.com>
2024-09-26 14:33:05 +0530

    style changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,10 +6,10 @@ const Home = ()=>{
     return (
         <>
         {/* <h1>choose game type</h1> */}
-        <div className="flex flex-col items-center justify-center h-full gap-5 w-4/5 mx-auto">
-        <Link to={'/game'}><Button className="p-5 h-14 w-52 gap-5 hover:bg-white hover:text-black">VS AI<Bot size={30}></Bot></Button></Link>
-        <Link to={'/online'}><Button className="w-52 h-14 p-5 gap-5 hover:bg-white hover:text-black">Local multiplayer<Users size={30}></Users></Button></Link>
-        <Link to={'/local'}><Button className="w-52 h-14 p-5 gap-5 hover:bg-white hover:text-black">Online multiplayer<Globe size={30}></Globe></Button></Link>
+        <div className="flex flex-col items-center justify-center h-screen gap-5 w-4/5 mx-auto">
+        <Link to={'/game'}><Button className="p-5 h-14 w-52 gap-5 hover:bg-slate-300 hover:text-black">VS AI<Bot size={30}></Bot></Button></Link>
+        <Link to={'/online'}><Button className="w-52 h-14 p-5 gap-5 hover:bg-slate-300 hover:text-black">Local multiplayer<Users size={30}></Users></Button></Link>
+        <Link to={'/local'}><Button className="w-52 h-14 p-5 gap-5 hover:bg-slate-300 hover:text-black">Online multiplayer<Globe size={30}></Globe></Button></Link>
         </div>
         </>
     )
