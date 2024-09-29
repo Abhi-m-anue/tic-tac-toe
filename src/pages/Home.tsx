8dcbcba31104cea365e23f59932e69d896e94fce
Abhimanue <abhimanue80@gmail.com>
2024-09-29 22:27:03 +0530

    some ui improvements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,11 @@
 import '../App.css'
 import { Link } from "react-router-dom"
-import { Bot, Users, Globe } from 'lucide-react';
+import { Bot, Globe } from 'lucide-react';
 import { Button } from "@/components/ui/button";
 
 const Home = ()=>{
     return (
         <>
-        {/* <h1>choose game type</h1> */}
         <div className="flex flex-col items-center justify-center h-5/6 gap-5 w-4/5 mx-auto">
         <Link to={'/game'}><Button className="p-5 h-14 w-52 gap-5 hover:bg-slate-300 hover:text-black slide-to-right">VS AI<Bot size={30}></Bot></Button></Link>
         {/* <Link to={'/online'}><Button className="w-52 h-14 p-5 gap-5 hover:bg-slate-300 hover:text-black">Local multiplayer<Users size={30}></Users></Button></Link> */}
